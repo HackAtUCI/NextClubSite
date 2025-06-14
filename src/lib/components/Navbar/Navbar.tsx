ab10efeee5cf44e7704d4488df87e42fe4b6ca3c
ianbb <ianbbdai@gmail.com>
2025-06-13 20:20:32 -0700

    Used next/link for navbar

--- src/lib/components/Navbar/Navbar.tsx
+++ src/lib/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import HackLogo from "@/assets/logos/hack_navbar.svg";
 import "./Navbar.css";
 
@@ -14,22 +15,25 @@ const Navbar: React.FC = () => {
   return (
     <nav className="w-full flex items-center justify-between px-16 py-4 absolute">
       <div className="flex items-center">
-        <Image
-          src={HackLogo.src}
-          alt="Hack at UCI Logo"
-          width={60}
-          height={60}
-        />
+        <Link href="/">
+          <Image
+            src={HackLogo.src}
+            alt="Hack at UCI Logo"
+            width={60}
+            height={60}
+            style={{ cursor: "pointer" }}
+          />
+        </Link>
       </div>
       <div className="flex gap-12">
         {navLinks.map((link) => (
-          <a
+          <Link
             key={link.href}
             href={link.href}
             className="navMenuLink font-semibold"
           >
             {link.label}
-          </a>
+          </Link>
         ))}
       </div>
     </nav>

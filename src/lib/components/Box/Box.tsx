3cf02de6f93dc3b96ca0558325666f4a9a8c58cd
ianbb <ianbbdai@gmail.com>
2025-06-27 22:39:32 -0700

    Made Box component mobile-responsive

--- src/lib/components/Box/Box.tsx
+++ src/lib/components/Box/Box.tsx
@@ -8,7 +8,7 @@ interface BoxProps {
 const Box: React.FC<BoxProps> = ({ children, className = "" }) => {
   return (
     <div
-      className={`mx-24 px-16 py-12 rounded-3xl bg-box shadow-box-glow ${className}`}
+      className={`mx-4 p-10 md:mx-24 md:px-16 md:py-12 rounded-3xl bg-box shadow-box-glow ${className}`}
     >
       {children}
     </div>

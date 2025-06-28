49729e30b7331abb69662b5f3796b745ff0b3cb0
ianbb <ianbbdai@gmail.com>
2025-06-27 18:07:55 -0700

    Updated text-heading to be smaller for mobile screens

--- src/lib/components/Home/Hero/Hero.tsx
+++ src/lib/components/Home/Hero/Hero.tsx
@@ -5,17 +5,18 @@ import PrimaryButton from "@/lib/components/PrimaryButton/PrimaryButton";
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex items-center justify-center gap-24 h-screen w-full">
+    <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-8 lg:gap-24 h-screen w-full">
       <div className="flex items-center justify-center">
         <Image
           src={HackLogo.src}
           alt="Hack at UCI Logo"
           width={300}
           height={300}
+          className="w-[30%] lg:w-[300px]"
         />
       </div>
       <div className="flex flex-col items-center justify-center gap-8">
-        <h1 className="text-heading">Hack at UCI</h1>
+        <h1 className="text-heading text-center">Hack at UCI</h1>
         <PrimaryButton className="px-32">Get Involved</PrimaryButton>
       </div>
     </div>

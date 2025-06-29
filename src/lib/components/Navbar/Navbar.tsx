98ace801620c9851ad4d3838433cff102764e642
ianbb <ianbbdai@gmail.com>
2025-06-29 00:12:29 -0700

    Used mui instead of react-bootstrap

--- src/lib/components/Navbar/Navbar.tsx
+++ src/lib/components/Navbar/Navbar.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
 import HackLogo from "@/assets/logos/hack_navbar.svg";
 import useScrollFromTop from "@/lib/hooks/useScrollFromTop";
 
@@ -19,49 +26,169 @@ const navLinks = [
 
 export default function NavBar() {
     const hasScrolledFromTop = useScrollFromTop();
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+        null
+    );
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
         <div
             className={`nav-container ${hasScrolledFromTop ? "" : "bg-no-scroll"}`}
         >
-            <Navbar
-                variant="dark"
-                expand="lg"
-                className="navbar-style"
+            <AppBar
+                position="static"
+                sx={{
+                    backgroundColor: "transparent",
+                    boxShadow: "none",
+                }}
             >
-                <Container fluid>
-                    <Navbar.Brand
-                        href="/"
-                        as={Link}
-                        className="logoBrand pl-4"
-                    >
-                        <div className="logo">
-                            <Image
-                                src={HackLogo}
-                                alt="Hack at UCI Logo"
-                                fill
-                            />
-                        </div>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse
-                        id="basic-navbar-nav"
-                        className="pr-6"
-                    >
-                        <Nav className="ms-auto">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        {/* Logo for desktop */}
+                        <Box
+                            sx={{ display: { xs: "none", md: "flex" }, mr: 2 }}
+                        >
+                            <Link
+                                href="/"
+                                style={{ textDecoration: "none" }}
+                            >
+                                <div
+                                    className="logo"
+                                    style={{
+                                        width: "35px",
+                                        height: "35px",
+                                        position: "relative",
+                                    }}
+                                >
+                                    <Image
+                                        src={HackLogo}
+                                        alt="Hack at UCI Logo"
+                                        fill
+                                        style={{ objectFit: "contain" }}
+                                    />
+                                </div>
+                            </Link>
+                        </Box>
+
+                        {/* Mobile menu button */}
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: "flex", md: "none" },
+                            }}
+                        >
+                            <IconButton
+                                size="large"
+                                aria-label="navigation menu"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                sx={{ color: "white" }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "left",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "left",
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: "block", md: "none" },
+                                    "& .MuiPaper-root": {
+                                        backgroundColor: "rgba(0, 0, 0, 0.9)",
+                                        color: "white",
+                                        border: "1px solid rgba(255, 255, 255, 0.1)",
+                                    },
+                                }}
+                            >
+                                {navLinks.map((link) => (
+                                    <MenuItem
+                                        key={link.href}
+                                        onClick={handleCloseNavMenu}
+                                        component={Link}
+                                        href={link.href}
+                                        sx={{
+                                            textAlign: "center",
+                                            "&:hover": {
+                                                backgroundColor:
+                                                    "rgba(255, 255, 255, 0.1)",
+                                            },
+                                        }}
+                                    >
+                                        <Typography
+                                            sx={{ textAlign: "center" }}
+                                        >
+                                            {link.label}
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+
+                        {/* Logo for mobile */}
+                        <Box
+                            sx={{ display: { xs: "flex", md: "none" }, mr: 2 }}
+                        >
+                            <Link
+                                href="/"
+                                style={{ textDecoration: "none" }}
+                            >
+                                <div
+                                    className="logo"
+                                    style={{
+                                        width: "50px",
+                                        height: "50px",
+                                        position: "relative",
+                                    }}
+                                >
+                                    <Image
+                                        src={HackLogo}
+                                        alt="Hack at UCI Logo"
+                                        fill
+                                        style={{ objectFit: "contain" }}
+                                    />
+                                </div>
+                            </Link>
+                        </Box>
+
+                        {/* Desktop navigation links */}
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: "none", md: "flex" },
+                                justifyContent: "flex-end",
+                            }}
+                        >
                             {navLinks.map((link) => (
                                 <Link
                                     key={link.href}
                                     href={link.href}
                                     className="navlink"
+                                    onClick={handleCloseNavMenu}
                                 >
                                     {link.label}
                                 </Link>
                             ))}
-                        </Nav>
-                    </Navbar.Collapse>
+                        </Box>
+                    </Toolbar>
                 </Container>
-            </Navbar>
+            </AppBar>
         </div>
     );
 }

0608f73fde9105aff1dbdc0728f6d244cce99a66
ianbb <ianbbdai@gmail.com>
2025-07-01 21:34:40 -0700

    Refactored Tabs component to add any component to bottom

--- src/app/recruitment/components/Committees.tsx
+++ src/app/recruitment/components/Committees.tsx
@@ -1,4 +1,6 @@
 import Tabs, { TabInterface } from "@/lib/components/Tabs/Tabs";
+import Box from "@/lib/components/Box/Box";
+// import PrimaryButton from "../PrimaryButton/PrimaryButton";
 import CorporateText from "./committee-texts/CorporateText";
 import LogisticsText from "./committee-texts/LogisticsText";
 import MarketingText from "./committee-texts/MarketingText";
@@ -6,33 +8,83 @@ import GraphicsText from "./committee-texts/GraphicsText";
 import TechnologyText from "./committee-texts/TechnologyText";
 
 const committeeTabs: TabInterface[] = [
-  {
-    buttonText: "Corporate",
-    tabTextComponent: <CorporateText />,
-  },
-  {
-    buttonText: "Logistics",
-    tabTextComponent: <LogisticsText />,
-  },
-  {
-    buttonText: "Marketing",
-    tabTextComponent: <MarketingText />,
-  },
-  {
-    buttonText: "Graphics",
-    tabTextComponent: <GraphicsText />,
-  },
-  {
-    buttonText: "Technology",
-    tabTextComponent: <TechnologyText />,
-  },
+    {
+        buttonText: "Corporate",
+        tabComponent: (
+            <RecruitmentTab
+                headingText="Corporate"
+                textComponent={<CorporateText />}
+            />
+        ),
+    },
+    {
+        buttonText: "Logistics",
+        tabComponent: (
+            <RecruitmentTab
+                headingText="Logistics"
+                textComponent={<LogisticsText />}
+            />
+        ),
+    },
+    {
+        buttonText: "Marketing",
+        tabComponent: (
+            <RecruitmentTab
+                headingText="Marketing"
+                textComponent={<MarketingText />}
+            />
+        ),
+    },
+    {
+        buttonText: "Graphics",
+        tabComponent: (
+            <RecruitmentTab
+                headingText="Graphics"
+                textComponent={<GraphicsText />}
+            />
+        ),
+    },
+    {
+        buttonText: "Technology",
+        tabComponent: (
+            <RecruitmentTab
+                headingText="Technology"
+                textComponent={<TechnologyText />}
+            />
+        ),
+    },
 ];
 
+function RecruitmentTab({
+    textComponent,
+    headingText,
+}: {
+    textComponent: React.ReactNode;
+    headingText: string;
+}) {
+    return (
+        <>
+            <Box className="pt-18 pb-24">
+                <h3 className="text-subtitle mb-8">{headingText}</h3>
+                {textComponent}
+            </Box>
+            {/* <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2">
+                <PrimaryButton
+                    className="px-[10rem]"
+                    variant="apply"
+                >
+                    Apply
+                </PrimaryButton>
+            </div> */}
+        </>
+    );
+}
+
 export default function Committees() {
-  return (
-    <section className="py-30">
-      <h2 className="text-subtitle text-center">Our Committees</h2>
-      <Tabs tabs={committeeTabs} />
-    </section>
-  );
+    return (
+        <section className="py-30">
+            <h2 className="text-subtitle text-center">Our Committees</h2>
+            <Tabs tabs={committeeTabs} />
+        </section>
+    );
 }
--- src/lib/components/Tabs/Tabs.tsx
+++ src/lib/components/Tabs/Tabs.tsx
@@ -2,54 +2,45 @@
 
 import { useState } from "react";
 
-import Box from "@/lib/components/Box/Box";
-// import PrimaryButton from "../PrimaryButton/PrimaryButton";
-
 import "./Tabs.css";
 
 export interface TabInterface {
-  buttonText:
-    | "Corporate"
-    | "Logistics"
-    | "Marketing"
-    | "Graphics"
-    | "Technology";
-  tabTextComponent: React.ReactNode;
+    buttonText:
+        | "Corporate"
+        | "Logistics"
+        | "Marketing"
+        | "Graphics"
+        | "Technology";
+    tabComponent: React.ReactNode;
 }
 
 interface TabsInterface {
-  tabs: TabInterface[];
+    tabs: TabInterface[];
 }
 
 export default function Tabs({ tabs }: TabsInterface) {
-  const [activeTab, setActiveTab] = useState<TabInterface | undefined>(tabs[0]);
+    const [activeTab, setActiveTab] = useState<TabInterface | undefined>(
+        tabs[0]
+    );
 
-  return (
-    <div>
-      <div className="flex justify-center items-center my-12">
-        {tabs.map((tab: TabInterface) => (
-          <button
-            key={tab.buttonText}
-            onClick={() => setActiveTab(tab)}
-            className={`tab-button ${
-              activeTab?.buttonText === tab.buttonText ? "active" : ""
-            }`}
-          >
-            {tab.buttonText}
-          </button>
-        ))}
-      </div>
-      <div className="relative">
-        <Box className="pt-18 pb-24">
-          <h3 className="text-subtitle mb-8">{activeTab?.buttonText}</h3>
-          {activeTab?.tabTextComponent}
-        </Box>
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2">
-          {/* <PrimaryButton className="px-[10rem]" variant="apply">
-            Apply
-          </PrimaryButton> */}
+    return (
+        <div>
+            <div className="my-12 flex items-center justify-center">
+                {tabs.map((tab: TabInterface) => (
+                    <button
+                        key={tab.buttonText}
+                        onClick={() => setActiveTab(tab)}
+                        className={`tab-button ${
+                            activeTab?.buttonText === tab.buttonText
+                                ? "active"
+                                : ""
+                        }`}
+                    >
+                        {tab.buttonText}
+                    </button>
+                ))}
+            </div>
+            <div className="relative">{activeTab?.tabComponent}</div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }

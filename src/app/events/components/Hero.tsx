0ae2b8e9e71fbde4da8087746c1c3909c776caf8
ianbb <ianbbdai@gmail.com>
2025-07-01 21:12:43 -0700

    Added event hero for real

---
+++ src/app/events/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <section className="flex h-screen flex-col items-center justify-center gap-12">
+            <h1 className="text-heading">Events</h1>
+        </section>
+    );
+}

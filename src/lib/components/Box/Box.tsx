9dfa8a931b82faf52b8735542bd6f5b0a5a5fb76
ianbb <ianbbdai@gmail.com>
2025-07-02 22:11:38 -0700

    Added alt box style, could not figure out why css style didn't apply when importing Box.css

--- src/lib/components/Box/Box.tsx
+++ src/lib/components/Box/Box.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import "./Box.css";
 
 interface BoxProps {
     children: React.ReactNode;
     className?: string;
+    alt?: boolean;
 }
 
-const Box: React.FC<BoxProps> = ({ children, className = "" }) => {
+const Box: React.FC<BoxProps> = ({ children, className = "", alt }) => {
     return (
         <div
-            className={`bg-box shadow-box-glow mx-4 rounded-3xl p-10 md:mx-24 md:px-16 md:py-12 ${className}`}
+            className={`${alt ? "box-style bg-[#232332]" : "bg-box shadow-box-glow"} mx-4 rounded-3xl p-10 md:mx-24 md:px-16 md:py-12 ${className}`}
         >
             {children}
         </div>
